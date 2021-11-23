6dd625cc4855905487b57555da5143a34d023fd4
chrisbel.simisterra <chrisbel.simisterra@pucese.edu.ec>
2021-11-23 17:00:01 -0500

    De binario a Decimal

--- EjemploFunciones/Convierte un numero en binario.Designer.cs
+++ EjemploFunciones/Convierte un numero en binario.Designer.cs
@@ -39,8 +39,9 @@ namespace EjemploFunciones
             this.checkBox8 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.Entero = new System.Windows.Forms.Label();
-            this.txtEntero = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConversion = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -51,6 +52,7 @@ namespace EjemploFunciones
             this.checkBox1.Size = new System.Drawing.Size(18, 17);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox2
             // 
@@ -60,6 +62,7 @@ namespace EjemploFunciones
             this.checkBox2.Size = new System.Drawing.Size(18, 17);
             this.checkBox2.TabIndex = 1;
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox3
             // 
@@ -69,6 +72,7 @@ namespace EjemploFunciones
             this.checkBox3.Size = new System.Drawing.Size(18, 17);
             this.checkBox3.TabIndex = 2;
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox4
             // 
@@ -78,6 +82,7 @@ namespace EjemploFunciones
             this.checkBox4.Size = new System.Drawing.Size(18, 17);
             this.checkBox4.TabIndex = 3;
             this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox5
             // 
@@ -87,6 +92,7 @@ namespace EjemploFunciones
             this.checkBox5.Size = new System.Drawing.Size(18, 17);
             this.checkBox5.TabIndex = 4;
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox6
             // 
@@ -96,6 +102,7 @@ namespace EjemploFunciones
             this.checkBox6.Size = new System.Drawing.Size(18, 17);
             this.checkBox6.TabIndex = 7;
             this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox7
             // 
@@ -105,6 +112,7 @@ namespace EjemploFunciones
             this.checkBox7.Size = new System.Drawing.Size(18, 17);
             this.checkBox7.TabIndex = 6;
             this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // checkBox8
             // 
@@ -114,6 +122,7 @@ namespace EjemploFunciones
             this.checkBox8.Size = new System.Drawing.Size(18, 17);
             this.checkBox8.TabIndex = 5;
             this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.ck1_CheckedChanged);
             // 
             // button1
             // 
@@ -134,12 +143,12 @@ namespace EjemploFunciones
             this.Entero.TabIndex = 9;
             this.Entero.Text = "Ingresa un entero ";
             // 
-            // txtEntero
+            // txtNumero
             // 
-            this.txtEntero.Location = new System.Drawing.Point(223, 65);
-            this.txtEntero.Name = "txtEntero";
-            this.txtEntero.Size = new System.Drawing.Size(311, 22);
-            this.txtEntero.TabIndex = 10;
+            this.txtNumero.Location = new System.Drawing.Point(223, 65);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(311, 22);
+            this.txtNumero.TabIndex = 10;
             // 
             // groupBox1
             // 
@@ -150,12 +159,23 @@ namespace EjemploFunciones
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Equivalente en binario";
             // 
+            // btnConversion
+            // 
+            this.btnConversion.Location = new System.Drawing.Point(513, 360);
+            this.btnConversion.Name = "btnConversion";
+            this.btnConversion.Size = new System.Drawing.Size(198, 78);
+            this.btnConversion.TabIndex = 12;
+            this.btnConversion.Text = "Convertir de binario a decimal ";
+            this.btnConversion.UseVisualStyleBackColor = true;
+            this.btnConversion.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Convierte_un_numero_en_binario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtEntero);
+            this.Controls.Add(this.btnConversion);
+            this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.Entero);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox6);
@@ -186,7 +206,8 @@ namespace EjemploFunciones
         private System.Windows.Forms.CheckBox checkBox8;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label Entero;
-        private System.Windows.Forms.TextBox txtEntero;
+        private System.Windows.Forms.TextBox txtNumero;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConversion;
     }
 }
--- EjemploFunciones/Convierte un numero en binario.cs
+++ EjemploFunciones/Convierte un numero en binario.cs
@@ -19,15 +19,39 @@ namespace EjemploFunciones
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num = int.Parse(this.txtEntero.Text);
+            bool esNumcorrecto = int.TryParse(this.txtNumero.Text, out int num);
+            if (!esNumcorrecto)
+            {
+                MessageBox.Show("Debe ingresar un valor numerico...");
+                return;
+            }
+            if (num > 0 && num <= 255)
+            {
+                MessageBox.Show("Numero fuera de rango...");
+                return;
+            }
             int[] res = Dec_Binario(num);
             Console.WriteLine("Inicio");
-            for(int i=0; i<res.Length; i++)
+            for (int i = res.Length; i >= 0; i--)
             {
                 Console.WriteLine("{0}", res[i]);
             }
             Console.WriteLine("Fin");
-            this.checkBox1.Checked = true;
+
+            /*
+            if (res[0] == 1)
+                this.checkBox8.Checked = true;
+            else
+                this.checkBox8.Checked = false;
+            */
+            this.checkBox8.Checked = res[7] == 1 ? true : false;
+            this.checkBox7.Checked = res[6] == 1 ? true : false;
+            this.checkBox6.Checked = res[5] == 1 ? true : false;
+            this.checkBox5.Checked = res[4] == 1 ? true : false;
+            this.checkBox4.Checked = res[3] == 1 ? true : false;
+            this.checkBox3.Checked = res[2] == 1 ? true : false;
+            this.checkBox2.Checked = res[1] == 1 ? true : false;
+            this.checkBox1.Checked = res[0] == 1 ? true : false;
         }
 
         private int[] Dec_Binario(int num)
@@ -36,14 +60,41 @@ namespace EjemploFunciones
             int i = 0;
             int res = 0;
 
-                while (num > 0)
-                {
-                    res = num % 2;
-                    bin[i++] = res;
-                    num = num / 2;      
+            while (num > 0)
+            {
+                res = num % 2;
+                bin[i++] = res;
+                num = num / 2;
                 //Console.WriteLine("{0}", res);
-                }
+            }
             return bin;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sumaBinarioDecimal();
+        }
+        void sumaBinarioDecimal()
+        {
+            int num = 0;
+            if (checkBox1.Checked) num += 1;
+            if (checkBox2.Checked) num += 2;
+            if (checkBox3.Checked) num += 4;
+            if (checkBox4.Checked) num += 8;
+            if (checkBox5.Checked) num += 16;
+            if (checkBox6.Checked) num += 32;
+            if (checkBox7.Checked) num += 64;
+            if (checkBox8.Checked) num += 128;
+            this.txtNumero.Text = num.ToString();
+        }
+
+        private void ck1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+  
 }
+
+
+
